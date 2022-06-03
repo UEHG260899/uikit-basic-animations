186280c582ca035a979c1abec752c88ccf2b2c8a
Uriel Enrique <urielenrique99@gmail.com>
2022-06-02 20:45:00 -0500

    animating constraint multipliers

--- PackingList/ViewController.swift
+++ PackingList/ViewController.swift
@@ -33,54 +33,110 @@
 import UIKit
 
 final class ViewController: UIViewController {
-  //MARK:- IB Outlets
-  
-  @IBOutlet private var tableView: TableView! {
-    didSet { tableView.handleSelection = showItem }
-  }
-
-  @IBOutlet private var menuButton: UIButton!
-  @IBOutlet private var titleLabel: UILabel!
-
-  //MARK:- Variables
-  
-  private var slider: HorizontalItemSlider!
-  private var menuIsOpen = false
+    //MARK:- IB Outlets
+    
+    @IBOutlet private var tableView: TableView! {
+        didSet { tableView.handleSelection = showItem }
+    }
+    
+    @IBOutlet private var menuButton: UIButton!
+    @IBOutlet private var titleLabel: UILabel!
+    
+    @IBOutlet private var menuHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private var menuItemTrilingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet private var titleCenterYClosedConstraint: NSLayoutConstraint!
+    @IBOutlet private var titleCenterYOpenConstrint: NSLayoutConstraint!
+    
+    //MARK:- Variables
+    
+    private var slider: HorizontalItemSlider!
+    private var menuIsOpen = false
 }
 
 //MARK:- UIViewController
 extension ViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    slider = HorizontalItemSlider(in: view) { [unowned self] item in
-      self.tableView.addItem(item)
-      self.transitionCloseMenu()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        slider = HorizontalItemSlider(in: view) { [unowned self] item in
+            self.tableView.addItem(item)
+            self.transitionCloseMenu()
+        }
+        titleLabel.superview!.addSubview(slider)
     }
-    titleLabel.superview!.addSubview(slider)
-  }
 }
 
 //MARK:- private
 private extension ViewController {
-  @IBAction func toggleMenu() {
-    menuIsOpen.toggle()
-  }
+    @IBAction func toggleMenu() {
+        menuIsOpen.toggle()
+        titleLabel.text = menuIsOpen ? "Select item!" : "Packing list"
+        view.layoutIfNeeded()
+        
+        
+        titleCenterYClosedConstraint.isActive = !menuIsOpen
+        titleCenterYOpenConstrint.isActive = menuIsOpen
+        
+        let constraints = titleLabel.superview!.constraints
+        constraints.first {
+            $0.firstItem === titleLabel && $0.firstAttribute == .centerX
+        }?.constant = menuIsOpen ? -100 : 0
+                
+        menuHeightConstraint.constant = menuIsOpen ? 200 : 80
+        menuItemTrilingConstraint.constant = menuIsOpen ? 16 : 8
+        
+        UIView.animate(withDuration: 1 / 3, delay: 0, options: .curveEaseIn) {
+            self.menuButton.transform = .init(rotationAngle: self.menuIsOpen ? .pi / 4 : 0)
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func showItem(_ item: Item) {
+        let imageView = UIImageView(item: item)
+        imageView.backgroundColor = .init(white: 0, alpha: 0.5)
+        imageView.layer.cornerRadius = 5
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        let bottomConstraint = imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: imageView.frame.height)
+        let centerXConstraint = imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let widthConstraint = imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: (1/3), constant: -50)
+        let heightConstraint = imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+        
+        
+        NSLayoutConstraint.activate([
+            bottomConstraint,
+            centerXConstraint,
+            widthConstraint,
+            heightConstraint
+        ])
+        
+        view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.8) {
+            bottomConstraint.constant = imageView.frame.height * -2
+            widthConstraint.constant = 0
+            self.view.layoutIfNeeded()
+        }
 
-  func showItem(_ item: Item) {
-    let imageView = UIImageView(item: item)
-    imageView.backgroundColor = .init(white: 0, alpha: 0.5)
-    imageView.layer.cornerRadius = 5
-    imageView.layer.masksToBounds = true
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(imageView)
-  }
+        UIView.animate(withDuration: 2 / 3, delay: 2) {
+            bottomConstraint.constant = imageView.frame.height
+            widthConstraint.constant = -50
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            imageView.removeFromSuperview()
+        }
 
-  func transitionCloseMenu() {
-    delay(seconds: 0.35, execute: toggleMenu)
-  }
+            
+    }
+    
+    func transitionCloseMenu() {
+        delay(seconds: 0.35, execute: toggleMenu)
+    }
 }
 
 private func delay(seconds: TimeInterval, execute: @escaping () -> Void) {
-  DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: execute)
+    DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: execute)
 }
